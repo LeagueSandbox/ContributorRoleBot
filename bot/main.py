08f3e62627271eb4302f7598d1f2016a240b7a48
Mythic <mythicmaniac@gmail.com>
2018-08-28 20:01:33 +0300

    Improve code style
    Improve code style in several parts
    
    No refs

--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,7 @@ import discord
 import asyncio
 
 DEBUG_ONLY = True
-INACTIVE_THRESHOLD = 15 # Defines how many days one has to be inactive to be counted inactive.
+INACTIVE_THRESHOLD = 15  # Defines how many days one has to be inactive to be counted inactive.
 
 GITHUB_TOKEN = ""
 DISCORD_TOKEN = ""
@@ -22,7 +22,8 @@ ContributorActivity = {}
 ContributorDiscords = {}
 GithubClient = Github(GITHUB_TOKEN)
 
-def InitOrRefreshActivity():
+
+def init_or_refresh_activity():
     print("Fetching all org repos... ", end='')
     repos = GithubClient.get_organization(OWNER).get_repos('public')
     print("done.")
@@ -37,7 +38,12 @@ def InitOrRefreshActivity():
             print("\r{}{} ".format(message, i), end='')
 
             # If our author is already marked as active
-            if commitTldr.author != None and commitTldr.author.login in ContributorActivity.keys() and ContributorActivity[commitTldr.author.login]:
+            author_is_active = (
+                commitTldr.author is None
+                and commitTldr.author.login in ContributorActivity.keys()
+                and ContributorActivity[commitTldr.author.login]
+            )
+            if author_is_active:
                 continue
 
             # If commit is a merge commit
@@ -48,7 +54,7 @@ def InitOrRefreshActivity():
             date = commitTldr.commit.author.date
             isOldCommit = datetime.now() - date > timedelta(days=INACTIVE_THRESHOLD)
 
-            if commitTldr.author != None and commitTldr.author.login in ContributorActivity.keys() and isOldCommit:
+            if commitTldr.author is None and commitTldr.author.login in ContributorActivity.keys() and isOldCommit:
                 continue
 
             # A second request is required to get files, as getting all commits doesn't send the change list
@@ -57,7 +63,7 @@ def InitOrRefreshActivity():
             # If the commit only edits *.md files
             files = commit.files
 
-            if files != None and all(file.filename.endswith(".md") for file in files):
+            if files is None and all(file.filename.endswith(".md") for file in files):
                 continue
 
             # If total lines edited is less than 10
@@ -68,15 +74,16 @@ def InitOrRefreshActivity():
             if totalChanges < 10:
                 continue
 
-            if commit.author != None:
+            if commit.author is None:
                 ContributorActivity[commit.author.login] = not isOldCommit
 
         print("done.")
 
     print("All done.")
-    ReadContributorFile()
+    read_contributor_file()
 
-def ReadContributorFile():
+
+def read_contributor_file():
     keys = ContributorActivity.keys()
     if os.path.exists("contributors.txt"):
         lines = []
@@ -89,7 +96,8 @@ def ReadContributorFile():
             if split[0] in keys:
                 ContributorDiscords[split[0]] = split[1]
 
-def FlushContributorFile():
+
+def flush_contributor_file():
     lines = []
     for key, value in ContributorDiscords.items():
         lines.append("{} {}".format(key, value))
@@ -98,35 +106,44 @@ def FlushContributorFile():
         for line in lines:
             file.write("{}\n".format(line))
 
+
 DISCORD_SERVER_ID = "166860156506865665"
 DISCORD_ACTIVE_ROLE_ID = "167012523814551552"
 DISCORD_INACTIVE_ROLE_ID = "259634105019269130"
 
 Client = discord.Client()
 
+
 @Client.event
 async def on_message(message):
     if message.content.lower().startswith('!printActivity'):
         tmp = ""
-        if not DEBUG_ONLY: tmp = await client.send_message(message.channel, "Refreshing contributor activity, this *might* take a while...")
-        InitOrRefreshActivity()
+        if not DEBUG_ONLY:
+            tmp = await Client.send_message(
+                message.channel,
+                "Refreshing contributor activity, this *might* take a while..."
+            )
+        init_or_refresh_activity()
         reply = ""
         for key, value in ContributorActivity.items():
             reply += "{} is {}active.\n".format(key, '' if value else 'in')
 
-        if not DEBUG_ONLY: await client.edit_message(tmp, reply)
+        if not DEBUG_ONLY:
+            await Client.edit_message(tmp, reply)
     elif message.content.lower().startswith('!forceRefresh'):
-        await UpdateRoles()
-        if not DEBUG_ONLY: await client.add_reaction(message, "✅")
+        await update()
+        if not DEBUG_ONLY:
+            await Client.add_reaction(message, "✅")
 
-async def Update():
-    InitOrRefreshActivity()
+
+async def update():
+    init_or_refresh_activity()
     for key, value in ContributorActivity.items():
         log("{} is {}active.\n".format(key, '' if value else 'in'))
 
     guild = Client.get_server(DISCORD_SERVER_ID)
 
-    if guild == None or guild.unavailable:
+    if guild is None or guild.unavailable:
         log("It seems like either we are not in the discord server or server is down.\n")
         return
 
@@ -140,50 +157,57 @@ async def Update():
 
     for user in guild.members:
         userId = user.id
-        # print("{} = {}".format(user.name, ["{}-{}".format(x.name, x.id) for x in user.roles]))
+
         if userId in ContributorDiscords.values():
             githubName = ""
             for key, value in ContributorDiscords.items():
                 if value == userId:
                     githubName = key
                     break
-            
+
             isActive = ContributorActivity[githubName]
 
-            if not inactiveRole in user.roles:
+            if inactiveRole not in user.roles:
                 log("A contributor ({}) is not marked as inactive contributor, adding role... ".format(user.name))
-                if not DEBUG_ONLY: await Client.add_roles(user, inactiveRole)
+                if not DEBUG_ONLY:
+                    await Client.add_roles(user, inactiveRole)
                 print("done.")
 
-            if isActive and not activeRole in user.roles:
+            if isActive and activeRole not in user.roles:
                 log("An active contributor ({}) doesn't have active role, adding role... ".format(user.name))
-                if not DEBUG_ONLY: await Client.add_roles(user, activeRole)
+                if not DEBUG_ONLY:
+                    await Client.add_roles(user, activeRole)
                 print("done.")
             elif not isActive and activeRole in user.roles:
                 log("An inactive contributor ({}) is marked as active, removing role... ".format(user.name))
-                if not DEBUG_ONLY: await Client.remove_roles(user, activeRole)
+                if not DEBUG_ONLY:
+                    await Client.remove_roles(user, activeRole)
                 print("done.")
         else:
             if activeRole in user.roles:
                 log("A non-contributor ({}) is marked as active contributor, removing role... ".format(user.name))
-                if not DEBUG_ONLY: await Client.remove_roles(user, activeRole)
+                if not DEBUG_ONLY:
+                    await Client.remove_roles(user, activeRole)
                 print("done.")
 
             if inactiveRole in user.roles:
                 log("A non-contributor ({}) is marked as inactive contributor, removing role... ".format(user.name))
-                if not DEBUG_ONLY: await Client.remove_roles(user, activeRole)
+                if not DEBUG_ONLY:
+                    await Client.remove_roles(user, activeRole)
                 print("done.")
+
 
 async def background_task():
     print("Welcome!")
     await Client.wait_until_ready()
 
     while True:
-        ReadContributorFile()
-        await Update()
-        await asyncio.sleep(900) # 15 minutes
+        read_contributor_file()
+        await update()
+        await asyncio.sleep(900)  # 15 minutes
 
-def log(text, fileOnly = False):
+
+def log(text, fileOnly=False):
     text = "[{}] {}".format(datetime.now(), text)
     with open("logs.txt", 'a+') as log_file:
         log_file.write(text if text.endswith('\n') else '{}\n'.format(text))
